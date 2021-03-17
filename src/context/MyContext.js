1f22561307d0cc2d55d07d6625f3b593b26a14ce
Dasha Lary <dasha4988@gmail.com>
2021-03-17 13:31:34 -0400

    began implementing useEffect hook

--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -6,5 +6,16 @@ export default MyContext
 /* now we need to make context available to all components via provider */
 
 function Provider(props) {
+    const [tasks, setTasks] = useState([])
 
+    useEffect(() => {
+        const fetchData = async() => {
+            const res = await fetch('/tasks')
+            const data = await res.json()
+            setTasks(data)
+        }
+        fetchData()
+    }, [])
+
+    
 }
