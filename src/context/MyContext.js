5be8f4f8102a644cc6a6c5659c2d0b2b70b607b1
Dasha Lary <dasha4988@gmail.com>
2021-03-17 13:22:51 -0400

    added comment

--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 const MyContext = React.createContext() // creating a context object
 export default MyContext
+
+/* now we need to make context available to all components via provider */
+
+function Provider(props) {
+
+}
