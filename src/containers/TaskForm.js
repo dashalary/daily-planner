e5d6689c3f8e1d51c096d44aa7eb60b510be18ee
Dasha Lary <dasha4988@gmail.com>
2021-03-19 16:53:20 -0400

    finished taskform

--- src/containers/TaskForm.js
+++ src/containers/TaskForm.js
@@ -1,6 +1,35 @@
 import React, {useState} from 'react';
+import MyContext from '../context/MyContext';
 
 function TaskForm() {
     const [description, setDescription] = useState("")
     const [complete, setComplete] = useState(false)
+
+    return (
+        <MyContext.Consumer>
+            {context => {
+            const handleOnChange = e => {
+                setDescription(e.target.value)
+            }
+            const handleOnSubmit = event => {
+                event.preventDefault()
+                context.addTask(description, complete)
+                setDescription("")
+                setComplete(false)
+            }
+
+            return (
+                <form onSubmit={handleOnSubmit}>
+                <input
+                type="text"
+                value={description}
+                onChange={handleOnChange} />
+                <button type="submit">Add Task</button>
+                </form>
+            )
+        }}
+        </MyContext.Consumer>
+    )
 }
+
+export default TaskForm
