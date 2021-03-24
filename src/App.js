7473eb8cf54f82c3102f81b2a21406bb103da6d9
Dasha Lary <dasha4988@gmail.com>
2021-03-24 18:32:56 -0400

    added styling and particles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Tasks from './containers/Tasks'
+import Particles from 'react-particles-js';
+import particlesParams from './particles-params'
 
 function App(props) {
   return (
     <div className="App">
+      <div style={{position: "absolute",  top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"}}>
+        <Particles params={particlesParams}/>
+      </div>
       <Tasks />
     </div>
   );
